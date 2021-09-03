68bc407a0a820e951b51d98966f3cf09d47661e0
Senju Nakajima <senjyu0626@yahoo.co.jp>
2021-09-03 15:00:11 +0900

    Loading.vueの追加

---
+++ front/topicslab/src/components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Card>
+        loading
+    </Card>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default {
+    name: 'loading',
+    data() {
+        return{
+            info: null,
+            loading: true,
+            errored: false
+        }
+    },
+}
+</script>
