f28bd9ed32250015857d00ccb9290e3dba957a0a
HareNishijima <39506116+HareNishijima@users.noreply.github.com>
2021-09-08 13:26:17 +0900

    Update Userself.vue
    項目番号26の再々修整
    ・自己紹介文を表示

--- front/topicslab/src/views/Userself.vue
+++ front/topicslab/src/views/Userself.vue
@@ -9,9 +9,9 @@
       <template #content>
         {{user.name}}
         <div class="p-field">
-          自己紹介文
+          自己紹介文：{{user.introduction}}
         <Textarea  v-model="introduction" :autoResize="true" rows="6" />
-        <Button icon="pi" iconPos="center" class="p-button-rounded p-button-help p-button-outlined p-button-sm" label="保存する" v-on:click="updateIntroduction (this.user.id,'自己紹介文')" />
+        <Button icon="pi" iconPos="center" class="p-button-rounded p-button-help p-button-outlined p-button-sm" label="保存する" v-on:click="updateIntroduction (this.user.id,introduction)" />
         </div>
       </template>
       <template #footer>
